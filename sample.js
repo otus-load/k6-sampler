724e6de3be7f38a9cf04d93a5f41bbdab13a6b30
Maksimall89 <Maksimall89@gmail.com>
2022-05-15 16:17:22 +0400

    feat: add eslint

--- sample.js
+++ sample.js
@@ -1,55 +1,63 @@
-import http from "k6/http";
-import { check, group } from "k6";
+import http from 'k6/http';
+import { check, group } from 'k6';
 
 const BASE_URL = 'https://test.k6.io';
 
-export let options = {
-    discardResponseBodies: true,
-    scenarios: {
-      base: {
-        executor: 'constant-vus',
-        exec: 'get_base',
-        vus: 50,
-        duration: '30s',
-      },
-      features: {
-        executor: 'per-vu-iterations',
-        exec: 'get_features',
-        vus: 10,
-        iterations: 100,
-        startTime: '10s',
-        maxDuration: '1m',
-      },
+export const options = {
+//   discardResponseBodies: true, // if you check only http-code
+  scenarios: {
+    base: {
+      executor: 'constant-vus',
+      exec: 'getBase',
+      vus: 50,
+      duration: '30s',
     },
-    thresholds: {
-        http_req_failed: ['rate<0.01'],
-        http_req_duration: ['p(95)<200'],
+    features: {
+      executor: 'per-vu-iterations',
+      exec: 'getFeatures',
+      vus: 10,
+      iterations: 100,
+      startTime: '10s',
+      maxDuration: '1m',
     },
+  },
+  thresholds: {
+    http_req_failed: ['rate<1'],
+    http_req_duration: ['p(95)<300'],
+  },
 };
 
-export default function(){
-    group("get_base", function(){get_base()});
-    group("get_features", function(){get_features()});
-};
+export function getBase() {
+  const res = http.get(BASE_URL);
+  check(res, {
+    'status code is 200': (res) => res.status === 200,
+  });
 
-export function get_base(){
-    let res = http.get(BASE_URL);
+  const payload = { name: 'Bert' };
+  const headers = { headers: { 'Content-Type': 'application/json' } };
+  const resPost = http.post(
+    'https://test.k6.io/flip_coin.php',
+    JSON.stringify(payload),
+    headers,
+  );
+  check(resPost, {
+    'status code is 200': (resPost) => resPost.status === 200,
+  });
+}
+
+export function getFeatures() {
+  group('Login', () => {
+    const res = http.get(`${BASE_URL}/news.php`);
     check(res, {
-        "status code is 200": (res) => res.status == 200,
+      'status code is not 404': (res) => res.status !== 404,
     });
+    const resMessage = http.get(`${BASE_URL}/my_messages.php`);
+    const title = resMessage.html().find('head title').text();
+    console.log(title);
+  });
+}
 
-    let data = { name: 'Bert' };
-    let resPost = http.post("https://test.k6.io/flip_coin.php", JSON.stringify(data),
-                        { headers: { 'Content-Type': 'application/json' } });
-};
-
-export function get_features(){
-    let res = http.get(BASE_URL + "/feature.php");
-    check(res, {
-        "status code is 404": (res) => res.status == 404,
-    });
-	http.get(BASE_URL + "/news.php");
-
-    console.log("outs test")
-
-};
+export default function () {
+  group('get_base', () => { getBase(); });
+  group('get_features', () => { getFeatures(); });
+}
